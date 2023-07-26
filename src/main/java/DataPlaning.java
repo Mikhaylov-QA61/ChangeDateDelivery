74790f360e5ad7187ceacd4044d1311c6a1dcf2c
Pers test <qa61test@gmail.com>
2023-07-27 00:20:43 +0400

    faker add

--- src/main/java/DataPlaning.java
+++ src/main/java/DataPlaning.java
@@ -1,5 +1,5 @@
 
-
+import com.github.javafaker.Faker;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -9,60 +9,27 @@ import java.util.Random;
 
 public class DataPlaning {
 
-
+    static Faker faker = new Faker(new Locale("ru"));
     public String generateDate(long addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     public String generateCity() {
-        List<String> massCity = new ArrayList<>();
-        massCity.add("Самара");
-        massCity.add("Москва");
-        massCity.add("Казань");
-        massCity.add("Иркутск");
-        massCity.add("Рязань");
-        massCity.add("Санкт-Петербург");
-        massCity.add("Ульяновск");
-        massCity.add("Сыктывкар");
-        massCity.add("Чебоксары");
-        int count = (int) Math.floor(Math.random()* massCity.size());
 
-        String city = massCity.get((int) count);
+        String city = faker.address().city();
         return city;
     }
 
     public static String generateName() {
-        List<String> massName = new ArrayList<>();
-        massName.add("Туров Игорь");
-        massName.add("Лазарев Григорий");
-        massName.add("Алексеев Роман");
-        massName.add("Харитонов Владислав");
-        massName.add("Горбунов Владимир");
-        massName.add("Шубин Виталий");
-        massName.add("Петров Кирилл");
-        massName.add("Киселев Роман");
-        massName.add("Абрамов Егор");
-        int count = (int) Math.floor(Math.random()* massName.size());
 
-        String name = massName.get((int) count);
+        String name = faker.name().firstName() + " " + faker.name().lastName();
 
         return name;
     }
 
     public static String generatePhone() {
-        List<String> massPhone = new ArrayList<>();
-        massPhone.add("+72785416636");
-        massPhone.add("+73359163514");
-        massPhone.add("+75223779482");
-        massPhone.add("+72469201676");
-        massPhone.add("+73944978305");
-        massPhone.add("+75483120007");
-        massPhone.add("+71288572414");
-        massPhone.add("+71040463528");
-        massPhone.add("+72526457644");
-        int count = (int) Math.floor(Math.random()* massPhone.size());
 
-        String phone = massPhone.get((int) count);
+        String phone = faker.phoneNumber().phoneNumber();
         return phone;
     }
 
